d8d4ab8a4215d5420c58c7b4579edc82ae838d18
ashtadsam <ashtadsam@gmail.com>
2020-05-08 23:28:22 +0430

    get excel from a date range

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 import GetExcel
+import datetime
+from dateutil import utils as date_utils
+
+
+DAYS = 14
 
 if __name__ == '__main__':
     # Enter a date : string
-    e = GetExcel.CrawlToDownload(date='2020-04-25')
-    e.goto('http://www.tsetmc.com/Loader.aspx?ParTree=15131F')
-    e.get_excel()
+    base = datetime.datetime.today()
+    date_list = [base - datetime.timedelta(days=x) for x in range(DAYS)]
+    for date in date_list:
+        if date_utils.datetime.isoweekday(date) == 4:
+            date_list.remove(date)
+            continue
+        if date_utils.datetime.isoweekday(date) == 5:
+            date_list.remove(date)
+            continue
+
+    for date in date_list:
+        e = GetExcel.CrawlToDownload(date=date.strftime("%Y-%m-%d"))
+        e.goto('http://www.tsetmc.com/Loader.aspx?ParTree=15131F')
+        e.get_excel()
     
